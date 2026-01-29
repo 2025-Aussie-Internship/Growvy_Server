8383a6268f35dadea9579fed1df3817b1112d258
yourim08 <a01048988580@gmail.com>
2026-01-29 04:03:57 +0000

    fix: update .gitignore and SecurityConfig logic

--- src/main/java/com/growvy/config/SecurityConfig.java
+++ src/main/java/com/growvy/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.growvy.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import com.growvy.filter.JwtFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,7 +36,8 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/api/**"
                         ).permitAll().anyRequest().authenticated()
-                );
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
